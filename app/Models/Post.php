6ea73cb40eb2c466c8ff136ce0aa03372a028274
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-12-05 18:59:23 +0700

    learn: Caching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -29,27 +29,22 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts/")))
-            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn ($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ));
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts/")))
+                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn ($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
     {
-        // base_path()          := '/' project root path
-        // app_path()           := '/app' dir path
-        // resource_path()      := '/resources' dir path
-        if (! file_exists($path = resource_path("posts/{$slug}.html"))) {
-            // return redirect('/');
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,12 @@
 
 <body>
     <article>
-        <?= $post; ?>
+        <h1><?= $post->title; ?></h1>
     </article>
+
+    <div>
+        <?= $post->body; ?>
+    </div>
 
     <a href="/">Go Back</a>
 </body>
