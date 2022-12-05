c4bebf53276cc2a80438a33901b4051b2e547b0e
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-12-05 15:16:32 +0700

    learn: FS resource manipulations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,15 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
 class Post
 {
+    public static function all()
+    {
+        return File::files(resource_path('posts/'));
+    }
+
     public static function find($slug)
     {
         // base_path()          := '/' project root path
         // app_path()           := '/app' dir path
         // resource_path()      := '/resources' dir path
         if (! file_exists($path = resource_path("posts/{$slug}.html"))) {
-            return redirect('/');
+            // return redirect('/');
+            throw new ModelNotFoundException();
         }
 
         return cache()->remember("posts.{$slug}", 1, fn() => file_get_contents($path));
--- routes/web.php
+++ routes/web.php
@@ -15,70 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome'); // ['foo' => 'bar']; // 'hello, world';
-    return view('posts');
+    $posts = Post::all();
+
+    ddd((string) $posts[0]->getContents());
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
 
-// Route::get('posts/{post}', function($slug) {
-//     // return $slug;
-
-//     // $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
-//     $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-//     // ddd($path);
-
-//     if (! file_exists($path)) {
-//         // ddd  := Dump, Die, Debug
-//         // ddd('File does not exist');
-
-//         // dd   := Dump and Die
-//         // dd('File does not exist');
-
-//         // abort(404);
-
-//         return redirect('/');
-//     }
-
-
-//     // $post = file_get_contents($path);
-
-//     // <https://laravel.com/docs/9.x/cache#the-cache-helper>
-//     // cache()->remember(key: String, sec: int, )
-//     // $post = cache()->remember("posts.{$slug}", now()->addMinites(20), function() use ($path) {
-//     // $post = cache()->remember("posts.{$slug}", 1200, function() use ($path) {
-//     //     var_dump('file_get_contents');
-//     //     return file_get_contents($path);
-//     // });
-
-//     $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-
-//     return view('post', [
-//         // 'post' => '<h1>Hello World</h1>' // $post
-//         'post' => $post
-//     ]);
-
-//     // where(<var>, <regex>)
-//     // '{post}', the 1st param of 'Route::get()'
-// })->where('post', '[A-z_\-]+');
-// // })->whereAlpha('post');
-
+// Find a post by its slug and pass it to a view called "post"
 Route::get('posts/{post}', function($slug) {
-    // Find a post by its slug and pass it to a view called "post"
-
-    $post = Post::find($slug);
-
     return view('post', [
-        'post' => $post
+        'post' => Post::find($slug)
     ]);
-
-    // if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-    //     return redirect('/');
-    // }
-
-    // $post = cache()->remember("posts.{$slug}", 1, fn() => file_get_contents($path));
-
-    // return view('post', [
-    //     'post' => $post
-    // ]);
 })->where('post', '[A-z_\-]+');
