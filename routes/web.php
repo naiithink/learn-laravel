27ff0913126b64892fff3cff6300c751b8f3cde4
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-12-05 18:40:40 +0700

    learn: Complete Post::all

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $posts = [];
-
-    $posts = collect(File::files(resource_path("posts/")))
-        ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn ($document) => new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        ));
-
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
     ]);
 });
 
